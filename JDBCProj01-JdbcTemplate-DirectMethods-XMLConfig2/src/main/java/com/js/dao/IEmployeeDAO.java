70895ce96de516aff88fa6294ee38a94fc5d339f
senthikumar1983@gmail.com <senthilkumar@localhost>
2022-07-10 18:41:31 +0530

    Commit

--- JDBCProj01-JdbcTemplate-DirectMethods-XMLConfig2/src/main/java/com/js/dao/IEmployeeDAO.java
+++
@@ -1,7 +0,0 @@
-package com.js.dao;
-
-public interface IEmployeeDAO {
-
-	public int getEmpCount();
-	public String getEmpNameByEno(int eno);
-}
--- JDBCProj01-JdbcTemplate-DirectMethods-XMLConfig2/src/main/java/com/js/service/IEmployeeMgmtService.java
+++
@@ -1,6 +0,0 @@
-package com.js.service;
-
-public interface IEmployeeMgmtService {
-	public int getEmpCount();
-	public String getEmpByName(int eno);
-}
